60ec60e6f3fbac8dcc16b6ef4bf7e2d387c1024a
mitia <abominable359@gmail.com>
2025-11-10 05:56:32 +0300

    I've done all the job

---
+++ src/MainGame/Main.java
@@ -0,0 +1,13 @@
+package MainGame;
+import QuizGame.Game;
+
+
+
+
+public class Main {
+    public static void main(String[] args){
+        Game quiz = new Game();
+        quiz.Start();
+
+    }
+}
